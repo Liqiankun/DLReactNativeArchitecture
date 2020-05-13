1ba0ada624882629f8dce662cef794c5ff476db3
qiankunli <qiankunli@yahoo.com>
2020-05-13 21:19:16 +0800

    animationEnabled

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,98 +17,101 @@ import BadgeItem from '../components/BadgeTabbarItem'
 import styles from '../styles'
 
 function _renderTabbarIcon(icon) {
-    return <Image source={icon} style={styles.tabBarIcon} />
+  return <Image source={icon} style={styles.tabBarIcon} />
 }
 
 const Stack = createStackNavigator()
 const Tab = createBottomTabNavigator()
 
 const StackNavigatorOptions = {
-    headerStyle: {
-        backgroundColor: '#fff',
-        borderBottomWidth: 0,
-    },
-    headerTintColor: '#5B99FA',
-    headerBackTitle: ' '
+  headerStyle: {
+    backgroundColor: '#fff',
+    borderBottomWidth: 0,
+  },
+  headerTintColor: '#5B99FA',
+  headerBackTitle: ' ',
 }
 
 
 const tabarIcons = {
-    Home: {
-        render: focused => {
-            return focused
-                ? _renderTabbarIcon(require('../images/tabbar_home_selected.png'))
-                : _renderTabbarIcon(require('../images/tabbar_home.png'))
-        }
-    },
-    Message: {
-        render: focused => {
-            return focused
-                ? _renderTabbarIcon(require('../images/tabbar_message_selected.png'))
-                : _renderTabbarIcon(require('../images/tabbar_message.png'))
-        }
-    },
-    Profile: {
-        render: focused => {
-            return <BadgeItem focused={focused} />
-        }
+  Home: {
+    render: focused => {
+      return focused
+        ? _renderTabbarIcon(require('../images/tabbar_home_selected.png'))
+        : _renderTabbarIcon(require('../images/tabbar_home.png'))
     }
+  },
+  Message: {
+    render: focused => {
+      return focused
+        ? _renderTabbarIcon(require('../images/tabbar_message_selected.png'))
+        : _renderTabbarIcon(require('../images/tabbar_message.png'))
+    }
+  },
+  Profile: {
+    render: focused => {
+      return <BadgeItem focused={focused} />
+    }
+  }
 }
 
 HomeStack = () => (
-    <Stack.Navigator
-        screenOptions={StackNavigatorOptions}
-    >
-        <Stack.Screen name="Home" component={HomePage} options={{ title: '首页' }} />
-        <Stack.Screen name="HomeDetail" component={HomeDetailPage} options={{ title: '详情' }} />
-    </Stack.Navigator>
+  <Stack.Navigator
+    screenOptions={StackNavigatorOptions}
+  >
+    <Stack.Screen name="Home" component={HomePage} options={{ title: '首页' }} />
+    <Stack.Screen name="HomeDetail" component={HomeDetailPage} options={{ title: '详情' }} />
+  </Stack.Navigator>
 )
 
 MessageStack = () => (
-    <Stack.Navigator
-        screenOptions={StackNavigatorOptions}
-    >
-        <Stack.Screen name="Message" component={MessagePage} options={{ headerShown: false }} />
-        <Stack.Screen name="MessageDetail" component={MessageDetailPage} options={{ headerShown: false }} />
-    </Stack.Navigator>
+  <Stack.Navigator
+    screenOptions={StackNavigatorOptions}
+  >
+    <Stack.Screen name="Message" component={MessagePage} options={{ headerShown: false, animationEnabled: false }} />
+    <Stack.Screen name="MessageDetail" component={MessageDetailPage} options={{ headerShown: false, animationEnabled: false }} />
+  </Stack.Navigator>
 )
 
 const TabBarStack = () => (
-    <Tab.Navigator
-        tabBarOptions={{
-            activeTintColor: '#6699ff',
-            inactiveTintColor: '#999',
-        }}
-        screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused }) => tabarIcons[route.name].render(focused),
-        })}
-    >
-        <Tab.Screen name="Home" component={HomeStack} options={({ route }) => ({
-            tabBarLabel: '首页',
-            tabBarVisible: route.state && route.state.index === 0
-        })} />
-        <Tab.Screen name="Message" component={MessageStack} options={{ tabBarLabel: '信息' }} />
-        <Tab.Screen name="Profile" component={ProfilePage} options={{ tabBarLabel: '我的' }} />
-    </Tab.Navigator>
+  <Tab.Navigator
+    tabBarOptions={{
+      activeTintColor: '#6699ff',
+      inactiveTintColor: '#999',
+    }}
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused }) => tabarIcons[route.name].render(focused),
+    })}
+  >
+    <Tab.Screen name="Home" component={HomeStack} options={({ route }) => ({
+      tabBarLabel: '首页',
+      tabBarVisible: route.state && route.state.index === 0
+    })} />
+    <Tab.Screen name="Message" component={MessageStack} options={{ tabBarLabel: '信息' }} />
+    <Tab.Screen name="Profile" component={ProfilePage} options={{ tabBarLabel: '我的' }} />
+  </Tab.Navigator>
 )
 
 AppStack = ({ isLaunching }) => (
-    <Stack.Navigator
-    >
-        {
-            isLaunching ?
-                <Stack.Screen name="Launch" component={LaunchPage} options={{ headerShown: false }} />
-                : <Stack.Screen name="TabBar" component={TabBarStack} options={{ headerShown: false }} />
-        }
-    </Stack.Navigator>
+  <Stack.Navigator
+    screenOptions={{
+      animationEnabled: false
+    }}
+  >
+    {
+      isLaunching ?
+        <Stack.Screen name="Launch" component={LaunchPage} options={{ headerShown: false, animationEnabled: false }} />
+        : <Stack.Screen name="TabBar" component={TabBarStack} options={{ headerShown: false }} />
+    }
+  </Stack.Navigator>
 )
 
 const SwitchStack = ({ isLaunching }) => (
-    <NavigationContainer>
-        <AppStack isLaunching={isLaunching} />
-    </NavigationContainer>
+  <NavigationContainer>
+    <AppStack isLaunching={isLaunching} />
+  </NavigationContainer>
 )
 
 export default connect((state) => ({
-    isLaunching: state.Login.isLaunching
+  isLaunching: state.Login.isLaunching
 }))(SwitchStack)
